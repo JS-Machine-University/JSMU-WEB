cea41dc462169215d653f061c95905cec7e17f9a
DaniWitcher <accforjavarush@gmail.com>
2023-01-10 22:16:54 +0300

    Button Created

--- projects/common-components/src/public-api.ts
+++ projects/common-components/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/common-components.service';
 export * from './lib/common-components.component';
+export * from './lib/custom-button/custom-button.component'
 export * from './lib/common-components.module';
