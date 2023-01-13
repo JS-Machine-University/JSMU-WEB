45b8ac0eac8d152decc79b8ff4e8f68e22295ae0
Kenan <haciyevk785@gmail.com>
2023-01-13 13:03:24 +0400

    app common configs

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+Handled configs of app, added some base styles to styles.scss
+By command npm run test-ci - we will checking our tests and we getting the coverage in console and coverage files too - for watching our results in browser too
+In husky file i wrote just one command in pre-commit - it is ng lint --fix
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { SharedModule } from '@jsmu/shared';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
+import { SharedModule } from "@jsmu/shared"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {CommonComponentsModule} from "common-components";
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { CommonComponentsModule } from "common-components"
+import { StoreModule } from "@ngrx/store"
+import { EffectsModule } from "@ngrx/effects"
+import { StoreDevtoolsModule } from "@ngrx/store-devtools"
+import { environment } from "../environments/environment"
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, CommonComponentsModule ],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		SharedModule,
+		CommonComponentsModule,
+		StoreModule.forRoot({}, {}),
+		EffectsModule.forRoot([]),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
