5c05bc1a205c966c18bd7416760247deebd99d6d
Kenan <haciyevk785@gmail.com>
2023-01-14 15:32:27 +0400

    some fixes of configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from "@angular/core"
-import { BrowserModule } from "@angular/platform-browser"
-import { SharedModule } from "@jsmu/shared"
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { SharedModule } from "@jsmu/shared";
 
-import { AppRoutingModule } from "./app-routing.module"
-import { AppComponent } from "./app.component"
-import { CommonComponentsModule } from "common-components"
-import { StoreModule } from "@ngrx/store"
-import { EffectsModule } from "@ngrx/effects"
-import { StoreDevtoolsModule } from "@ngrx/store-devtools"
-import { environment } from "../environments/environment"
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { CommonComponentsModule } from "@jsmu/common-components";
 
 @NgModule({
 	declarations: [AppComponent],
