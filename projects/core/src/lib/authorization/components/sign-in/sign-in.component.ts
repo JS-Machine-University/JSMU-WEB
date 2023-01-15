3486c701c5c401dd0d48c68d79502f2299bf1b57
DaniWitcher <accforjavarush@gmail.com>
2023-01-15 18:23:30 +0300

    fix(auth): removed interval added docheck instead

--- projects/core/src/lib/authorization/components/sign-in/sign-in.component.ts
+++ projects/core/src/lib/authorization/components/sign-in/sign-in.component.ts
@@ -1,36 +1,31 @@
-import { Component } from '@angular/core';
-import { AuthService } from 'projects/core/src/lib/authorization/services/auth/auth.service';
-import { MockDBService } from '../../services/mockDB/mock-db.service';
-import { interval } from 'rxjs';
+import { Component, DoCheck } from "@angular/core";
+import { AuthService } from "projects/core/src/lib/authorization/services/auth/auth.service";
+import { MockDBService } from "../../services/mockDB/mock-db.service";
+import { interval } from "rxjs";
 
 @Component({
-  selector: 'jsmu-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss'],
+	selector: "jsmu-sign-in",
+	templateUrl: "./sign-in.component.html",
+	styleUrls: ["./sign-in.component.scss"]
 })
-export class SignInComponent {
-  constructor(
-    public authService: AuthService,
-    public dataService: MockDBService
-  ) {}
+export class SignInComponent implements DoCheck {
+	constructor(public authService: AuthService, public dataService: MockDBService) {}
 
-  public loginUser(): void {
-    this.authService.gitHubAuth();
+	public loginUser(): void {
+		this.authService.gitHubAuth();
+	}
 
-    const timer = setInterval(() => {
-      if (this.authService.isLoggedIn) {
-        if (this.dataService.isUserPresent(this.authService.userData.uid)) {
-          clearInterval(timer);
-          //this.authService.routNavigate('dashboard'); toDo: Need page to relocate in future
-        } else {
-          clearInterval(timer);
-          this.authService.routNavigate('role-select');
-        }
-      }
-    }, 2000);
-  }
+	public ngDoCheck(): void {
+		if (this.authService.isLoggedIn) {
+			if (this.dataService.isUserPresent(this.authService.userData.uid)) {
+				//this.authService.routNavigate('dashboard'); toDo: Need page to relocate in future
+			} else {
+				this.authService.routNavigate("role-select");
+			}
+		}
+	}
 
-  public logoutUser(): void {
-    this.authService.signOut();
-  }
+	public logoutUser(): void {
+		this.authService.signOut();
+	}
 }
