48bcb54db35f3b042db0f1327c62e9f3faf8846f
Irada Mamedova <iradamamedova112@mail.ru>
2023-01-23 10:28:13 +0400

    fix(firebase): resolve conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,6 @@ import { UsersDataService } from "projects/core/src/services/users.data.service"
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  user1: User = {
-    uid: "1",
-    email: "2",
-    displayName: "ira",
-    photoURL: "ira.jpg",
-    emailVerified: false,
-  };
-  user2: User = {
-    uid: "2",
-    email: "3",
-    displayName: "irada",
-    photoURL: "irada.jpg",
-    emailVerified: true,
-  };
   constructor(private udb: UsersDataService) {}
   public saveUser(user: User) {
     this.udb.saveUser(user).subscribe();
