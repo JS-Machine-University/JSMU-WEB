2e2006e4048fd9018e0eea06ede2285ad292d277
Irada Mamedova <iradamamedova112@mail.ru>
2023-01-25 12:17:24 +0400

    feat(mentee-page): create mentee page with modules

--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from "@angular/core";
-import { CoreComponent } from "./core.component";
+import { NgModule } from '@angular/core';
+import { CoreComponent } from './core.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ModulesDataService } from './services/modules-data.service';
+import { ModulesService } from './services/modules.service';
 import { AuthorizationModule } from "./authorization/authorization.module";
 
 @NgModule({
-	declarations: [CoreComponent],
+  declarations: [CoreComponent],
+  imports: [HttpClientModule],
+  exports: [CoreComponent],
+  providers: [ModulesDataService, ModulesService],
 	imports: [AuthorizationModule],
-	exports: [CoreComponent]
 })
 export class CoreModule {}
