47149b9cfd4bb133ab23046407457ec93c08ac2c
Irada Mamedova <iradamamedova112@mail.ru>
2023-02-23 20:17:55 +0400

    test(home-page): fix tests according to last changes

--- projects/core/src/lib/components/home-page/home-page.component.spec.ts
+++ projects/core/src/lib/components/home-page/home-page.component.spec.ts
@@ -1,32 +1,18 @@
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { Observable } from "rxjs";
 import { HomePageSection } from "../../models/home-page-section";
-import { HomePageSectionsService } from "../../services/home-page-sections.service";
 import { HomePageComponent } from "./home-page.component";
+import sections from "../../../../../../src/assets/jsons/sections.json";
 
 describe("HomePageComponent", () => {
 	let component: HomePageComponent;
 	let fixture: ComponentFixture<HomePageComponent>;
-	const expectedSections = [
-		{ name: "1", title: "a" },
-		{ name: "2", title: "b" }
-	] as HomePageSection[];
-	class MockSectionsService {
-		public getSections(): Observable<HomePageSection[]> {
-			return new Observable<HomePageSection[]>((subscriber) => {
-				subscriber.next(expectedSections);
-			});
-		}
-	}
-	let mocksdb: MockSectionsService;
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [HomePageComponent],
-			providers: [HomePageSectionsService, MockSectionsService],
 			imports: [HttpClientTestingModule]
 		}).compileComponents();
-		mocksdb = TestBed.inject(MockSectionsService);
 		fixture = TestBed.createComponent(HomePageComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
@@ -37,9 +23,6 @@ describe("HomePageComponent", () => {
 	});
 
 	it("should get sections", () => {
-		mocksdb.getSections().subscribe((sections: HomePageSection[]) => {
-			component.sections = sections;
-		});
-		expect(component.sections).toEqual(expectedSections);
+		expect(component.sections).toEqual(sections);
 	});
 });
