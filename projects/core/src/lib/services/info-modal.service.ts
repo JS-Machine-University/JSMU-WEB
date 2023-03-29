9784024eca0c27050f7a02fb293364d2b957e0b7
Kanan Hajiyev <haciyevk785@gmail.com>
2023-03-29 14:32:01 +0400

    refactor(info-panel): renaming observables variables

--- projects/core/src/lib/services/info-modal.service.ts
+++ projects/core/src/lib/services/info-modal.service.ts
@@ -5,9 +5,9 @@ import { Subject } from "rxjs";
 	providedIn: "root"
 })
 export class InfoModalService {
-	public $serviceSubjectProperty: Subject<boolean> = new Subject<boolean>();
+	public serviceSubjectProperty$: Subject<boolean> = new Subject<boolean>();
 
 	public callNextOnSubject(isModalOpen: boolean): void {
-		this.$serviceSubjectProperty.next(isModalOpen);
+		this.serviceSubjectProperty$.next(isModalOpen);
 	}
 }
