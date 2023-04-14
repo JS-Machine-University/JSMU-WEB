6fcaabbc0ba1d535bf8e3e3d04ed1890dcda447b
Elnur Ocaqquliyev <elnur.ocaqquliyev@gmail.com>
2023-04-14 12:54:24 +0400

    ThirdCommit

--- projects/core/src/lib/models/takeTalk.ts
+++
@@ -1,9 +0,0 @@
-export interface takeTalk {
-    // 	lessonId: string;
-    // 	result: Result;
-    // 	inProgress: boolean;
-    // 	expertId: string;
-    // 	menteeId: string;
-    // 	submitDate: Date;
-    // 	resultDate: Date;
-     }
